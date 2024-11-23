559e483f5f95bd30b7c2db56f19da4f0a1d63ab7
jazmfg <jazmfg@outlook.com>
2024-11-23 16:30:15 -0600

    Day 1 & Day 2 of 100 days of code

---
+++ Section1/Day1.py
@@ -0,0 +1,8 @@
+# Día 1. Generador de nombres de bandas
+
+print("Bienvenido al generador de nombres de bandas.")
+
+city = input("¿Cómo se llama la ciudad donde creciste? "'\n')
+pet = input("¿Cómo se llama tu mascota? "'\n')
+
+print("El nombre de tu banda podría ser: " + city + " " + pet)
